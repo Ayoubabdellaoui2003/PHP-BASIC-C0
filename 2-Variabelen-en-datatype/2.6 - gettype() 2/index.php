3cbaaa58458cd093a0c45cbee53753c9b7c449f1
Ayoubabdellaoui2003 <2106670@talnet.nl>
2022-01-25 13:49:02 +0100

    2.6 2

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,23 @@
+<?php
+$string ="Dit is een string";
+echo gettype ($string);
+echo "<br>";
+$int =1;
+echo gettype ($int);
+echo "<br>";
+$float = 0.1;
+echo gettype ($float);
+echo "<br>";
+$boolean = true;
+echo gettype ($boolean);
+echo "<br>";
+$null = null;
+echo gettype ($null);
+
+
+
+
+
+
+
+?>
